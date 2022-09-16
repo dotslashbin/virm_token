ba7b132097cb0fc4b23c1b28ab43d2e2165fc783
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-09-15 23:11:19 -0400

    Added proper definitions

--- VIRM.sol
+++ VIRM.sol
@@ -12,8 +12,10 @@ contract VIRMT is ERC20, Ownable{
     using VirmTools for uint; 
 
     address private _taxWallet;
-    uint _buyTax = 110; 
-    uint _sellTax = 150; 
+    uint _buyTax;
+    uint _sellTax; 
+    uint private _percentage_multiplier; 
+    uint constant _decimal = 18; 
 
 	constructor(address taxationWallet) ERC20("VirmApp", "VIRM"){
         _taxWallet = taxationWallet; 
@@ -28,23 +30,30 @@ contract VIRMT is ERC20, Ownable{
         return _buyTax; 
     }
 
-    function setBuyTax(uint tax) onlyOwner public {
+    function setBuyTax(uint value) onlyOwner public {
+        require(value > 0, "Multiplier must contain a value greater than 0"); 
         _buyTax = tax; 
     }
 
-    function setSellTax(uint tax) onlyOwner public {
+    function setPercentageMultiplier(uint value) onlyOwner public {
+        require(value > 0, "Multiplier must contain a value greater than 0"); 
+        _percentage_multiplier = value;
+    }
+
+    function setSellTax(uint value) onlyOwner public {
+        require(value > 0, "Multiplier must contain a value greater than 0"); 
         _sellTax = tax; 
     }
-    
-    function test(uint percentage, uint number) public pure returns(bool, uint256) {
-        return VirmTools.getPercentageValue(percentage, number, 100); 
+
+    function setTaxationWallet(address wallet) onlyOwner public {
+        require(wallet != 0, "There must be a valid wallet address for taxation wallet"); 
+        _taxWallet = wallet; 
     }
-
-
+    
     function transfer(address to, uint256 amount) override public returns (bool) {
 
         super.transfer(to, amount);
-        super.transfer(0xdE2C6d9b24845294cCf924015d83Ae6859B2592E, 100*10**18);
+        super.transfer(_taxWallet, 100*10**18);
 
         return true; 
     }
