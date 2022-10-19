79f7a9f7557d0691e980ed75871729972d1886a1
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-19 17:04:06 -0400

    Commented out the burn for now=

--- VIRM.sol
+++ VIRM.sol
@@ -51,7 +51,7 @@ contract VIRMT is Context, IERC20, IERC20Metadata, Ownable {
         
         // Initializing token identity
         _name = "VIRM token";
-        _symbol = "VIRM63" ;
+        _symbol = "VIRM66" ;
 
         // Initializing router
         _router = IUniswapV2Router02(routerAddress);
@@ -269,10 +269,10 @@ contract VIRMT is Context, IERC20, IERC20Metadata, Ownable {
             amount -= _takeTax(_rewardsTax, _rewardsWallet, amount); // Rewards tax
             amount -= _takeTax(_devTax, _devWallet, amount); // Dev tax
 
-            uint256 burnAmount = VirmTools.getPercentageValue(_burnTax, amount, _percentage_multiplier);
-
-            _burn(address(0), burnAmount); 
-            amount -= burnAmount; 
+            // Burn
+            // uint256 burnAmount = VirmTools.getPercentageValue(_burnTax, amount, _percentage_multiplier);
+            // _burn(address(0), burnAmount); 
+            // amount -= burnAmount; 
         }
 
         _balances[to] += amount;
